adb5830ff304d9d18ffe66db12ae366852e94d4f
Ron <Ron@DESKTOP-LBO1NL0.local>
2024-10-31 16:59:42 +0200

    new messanger

---
+++ ClientChat/NicknameWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ClientChat
+{
+    public partial class NicknameWindow : Window
+    {
+        public string Nickname { get; private set; }
+
+        public NicknameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            Nickname = nicknameTextBox.Text;
+            if (string.IsNullOrEmpty(Nickname))
+            {
+                MessageBox.Show("Please enter a valid nickname.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                DialogResult = true;
+                Close();
+            }
+        }
+        private void msgTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                OKButton_Click(sender, e);
+            }
+        }
+
+      
+        private void nicknameTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
